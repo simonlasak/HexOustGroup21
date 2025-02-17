135ba11a34dc03951e1df27c98f9a9d092bfd014
simonlasak <146730378+simonlasak@users.noreply.github.com>
2025-02-17 22:32:46 +0000

    try number 4

---
+++ src/main/java/com/example/demo1/HelloController.java
@@ -0,0 +1,14 @@
+package main.java.com.example.demo1;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
