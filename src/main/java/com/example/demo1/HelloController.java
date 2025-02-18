9316f8e31c269bb6d95afc800f6a0aa7399de88a
Simon Lasak <simonlasak4@gmail.com>
2025-02-18 09:51:35 +0000

    good morning adam, hexoust base 3 provisional board

--- src/main/java/com/example/demo1/HelloController.java
+++ src/main/java/com/example/demo1/HelloController.java
@@ -2,13 +2,75 @@ package com.example.demo1;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.fxml.FXML;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.shape.Polygon;
+
+import static javafx.scene.paint.Color.BLACK;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private Polygon hex1;
+
+    @FXML
+    private Polygon hex10;
+
+    @FXML
+    private Polygon hex11;
+
+    @FXML
+    private Polygon hex12;
+
+    @FXML
+    private Polygon hex13;
+
+    @FXML
+    private Polygon hex14;
+
+    @FXML
+    private Polygon hex15;
+
+    @FXML
+    private Polygon hex16;
+
+    @FXML
+    private Polygon hex17;
+
+    @FXML
+    private Polygon hex18;
+
+    @FXML
+    private Polygon hex19;
+
+    @FXML
+    private Polygon hex2;
+
+    @FXML
+    private Polygon hex3;
+
+    @FXML
+    private Polygon hex4;
+
+    @FXML
+    private Polygon hex5;
+
+    @FXML
+    private Polygon hex6;
+
+    @FXML
+    private Polygon hex7;
+
+    @FXML
+    private Polygon hex8;
+
+    @FXML
+    private Polygon hex9;
+
+    @FXML
+    void getHexID(MouseEvent event) {
+        Polygon hexagon = (Polygon) event.getSource();
+        hexagon.setFill(BLACK);
     }
+
 }
