485ec43e1720bec4ca6bbfa917cf08b2ed6fe082
Simon Lasak <simonlasak4@gmail.com>
2025-04-06 21:25:05 +0100

    tests

---
+++ src/test/java/UIPackage/BoardLogicTest.java
@@ -0,0 +1,35 @@
+package UIPackage;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+//import static UIPackage.BoardLogic.redHexagons;
+import static org.junit.jupiter.api.Assertions.*;
+
+class BoardLogicTest {
+    @BeforeEach
+    void resetBoard() {
+        BoardLogic.clearBoard();
+    }
+
+    @Test
+    void testValidMoveOnEmptySpot() {
+        HexCube move = new HexCube(new Point(300, 300));
+        assertTrue(BoardLogic.isValidMove(move, true));
+    }
+
+    @Test
+    void testInvalidMoveOnOccupiedSpot() {
+        HexCube move = new HexCube(new Point(300, 300));
+        BoardLogic.addToList(move, true);
+        assertFalse(BoardLogic.isValidMove(move, false));
+    }
+
+    @Test
+    void testAddToListIncreasesRedSize() {
+        HexCube move = new HexCube(new Point(300, 300));
+        int before = BoardLogic.redHexagon;
+        BoardLogic.addToList(move, true);
+        assertEquals(before + 1, BoardLogic.listSize(true));
+    }
+}
